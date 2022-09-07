68e3c7a0a40e6d36e5a762a4e836b7ef7545bd79
F_D <den9152@yandex.ru>
2022-09-07 19:42:03 +0700

    Initial commit

---
+++ Parsim_string_v_int_07-09-2022.cpp
@@ -0,0 +1,21 @@
+﻿// Parsim_string_v_int_07-09-2022.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+// дописать дома нужно тру и кеч сделать при увеличении строки до уровня выхода 
+// из инта
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string first = "123456789";
+	string mas1;
+	cout << atoi(first.c_str());
+	for (size_t i = 0; i < 5; i++)
+	{
+		cout << atoi(first.c_str());
+		cout << endl;
+	}
+ 
+
+}
+
